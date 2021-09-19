2c8b379c9e529764d5199b36b2d525ec4bc9a68e
Munir Mahmud <munirmahmud6@gmail.com>
2021-09-19 11:12:02 +0600

    Profile settings updated

--- components/layouts/DefaultLayout.jsx
+++ components/layouts/DefaultLayout.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Head from './modules/Head';
-import HeaderMobile from '~/components/shared/headers/HeaderMobile';
-import DrawerMenu from '~/components/shared/drawers/DrawerMenu';
+import React from "react";
+import DrawerMenu from "~/components/shared/drawers/DrawerMenu";
+import HeaderMobile from "~/components/shared/headers/HeaderMobile";
+import Head from "./modules/Head";
 
 const DefaultLayout = ({ children }) => {
-    return (
-        <div id="martfury">
-            <Head />
-            <HeaderMobile />
-            {children}
-            <DrawerMenu />
-            <div id="loader-wrapper">
-                <div className="loader-section section-left"></div>
-                <div className="loader-section section-right"></div>
-            </div>
-        </div>
-    );
+  return (
+    <div id="e-needz">
+      <Head />
+      <HeaderMobile />
+      {children}
+      <DrawerMenu />
+      <div id="loader-wrapper">
+        <div className="loader-section section-left"></div>
+        <div className="loader-section section-right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default DefaultLayout;
--- components/shared/forms/FormAccountSettings.jsx
+++ components/shared/forms/FormAccountSettings.jsx
@@ -74,6 +74,67 @@ const FormAccountSettings = () => {
             />
           </div>
         </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="state">State</label>
+            <input
+              id="state"
+              className="form-control"
+              type="text"
+              placeholder="State"
+            />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="state">City</label>
+            <input
+              id="city"
+              className="form-control"
+              type="text"
+              placeholder="City"
+            />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="state">Zip Code</label>
+            <input
+              id="zip_code"
+              className="form-control"
+              type="number"
+              placeholder="zip_code"
+            />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="state">Country</label>
+            <input
+              id="country"
+              className="form-control"
+              type="text"
+              placeholder="country"
+            />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="state">Company Name</label>
+            <input
+              id="company_name"
+              className="form-control"
+              type="text"
+              placeholder="Company Name"
+            />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="customer_address_2">Profile Logo/Picture</label>
+            <input id="profile_picture" className="form-control" type="file" />
+          </div>
+        </div>
       </div>
       <div className="ps-form__submit text-center">
         <button className="ps-btn ps-btn--gray mr-3">Cancel</button>
